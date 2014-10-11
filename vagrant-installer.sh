8b78b09d6e7105fa64687595277a43f46cc6617e
Fabio Rehm <fgrehm@gmail.com>
2014-10-10 23:17:32 -0300

    Add vagrant installer

---
+++ vagrant-installer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NOTIFY_PORT="${1}"
+
+if [ -z $NOTIFY_PORT ]; then
+  echo 'No server port was provided to the `notify-send` HTTP client!' 1>&2
+  exit 1
+fi
+
+if [ -x /usr/bin/notify-send ]; then
+  echo 'notify-send HTTP client already installed, skipping'
+  exit 0
+fi
+
+curl -L https://github.com/fgrehm/notify-send-http/releases/download/v0.1.0/client | sudo tee /usr/bin/notify-send &>/dev/null
+sudo chmod +x /usr/bin/notify-send
+
+cat <<-STR >> /home/vagrant/.bashrc
+SERVER_IP=\$(ip route|awk '/default/ { print \$3 }')
+export NOTIFY_SEND_URL="http://\${SERVER_IP}:${NOTIFY_PORT}"
+STR
